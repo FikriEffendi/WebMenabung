a1990154ef8e3155e11967db3b80602a20025703
aerochops <150325960+aerochops@users.noreply.github.com>
2025-03-01 21:03:37 +0700

    membuat route untuk authentication

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::inertia('/', 'views/home/page-home');
+Route::inertia('/', 'views/home/page-home')->middleware('auth');
+
+Route::inertia('/login', 'auth/page-login')->name('login');
+Route::inertia('/register', 'auth/page-register');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
